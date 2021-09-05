6a42283ff51643256aca9d89edaec3471da857b3
Geovana <geovanamalta2013@gmail.com>
2021-09-05 02:05:53 -0300

    adicionando conexao.php

---
+++ conexao.php
@@ -0,0 +1,12 @@
+<?php
+
+$host="localhost";
+$port=3306;
+$socket="";
+$user="root";
+$password="";
+$dbname="formulario-jobs";
+
+$con = new mysqli($host, $user, $password, $dbname, $port, $socket);
+
+?>
